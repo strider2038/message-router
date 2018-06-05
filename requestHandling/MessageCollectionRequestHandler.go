5be3ce5820510066e8cb9130d0fd3dd7655f6c01
strider <igorlazarev85@yandex.ru>
2018-06-05 21:50:55 +0300

    handling all request situations

--- requestHandling/MessageCollectionRequestHandler.go
+++ requestHandling/MessageCollectionRequestHandler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"regexp"
 )
 
 type MessageCollectionRequestHandler struct {
@@ -11,7 +12,25 @@ type MessageCollectionRequestHandler struct {
 
 func (handler MessageCollectionRequestHandler) HandleRequest(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-	writer.WriteHeader(http.StatusBadRequest)
-	message, _ := json.Marshal(JsonResponse{"Request body cannot be empty"})
+	contentType := request.Header.Get("Content-Type")
+	contentTypeIsJson, _ := regexp.MatchString("application/json.*", contentType)
+
+	var messageTitle string
+
+	if request.Body == nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		messageTitle = "Request body cannot be empty"
+	} else if !contentTypeIsJson {
+		writer.WriteHeader(http.StatusBadRequest)
+		messageTitle = "Content type must be application/json"
+	} else if request.Method != "POST" || request.URL.Path != "/" {
+		writer.WriteHeader(http.StatusNotFound)
+		messageTitle = "Route not found"
+	} else {
+		writer.WriteHeader(http.StatusOK)
+		messageTitle = "All messages were successfully sent to queue"
+	}
+
+	message, _ := json.Marshal(JsonResponse{messageTitle})
 	io.WriteString(writer, string(message))
 }
--- requestHandling/MessageCollectionRequestHandler_test.go
+++ requestHandling/MessageCollectionRequestHandler_test.go
@@ -8,11 +8,13 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 )
 
 func TestHandleRequest_emptyBody_badRequestErrorReturned(t *testing.T) {
 	handler := MessageCollectionRequestHandler{}
 	request := httptest.NewRequest("POST", "/", nil)
+	request.Body = nil
 	writer := httptest.NewRecorder()
 
 	handler.HandleRequest(writer, request)
@@ -24,5 +26,73 @@ func TestHandleRequest_emptyBody_badRequestErrorReturned(t *testing.T) {
 	assert.Contains(t, string(responseBody), "title")
 	title := gjson.Get(string(responseBody), "title").String()
 	assert.Contains(t, title, "Request body cannot be empty")
+	assert.Contains(t, contentType, "application/json")
+}
+
+var invalidRouteCases = []struct {
+	method string
+	url    string
+}{
+	{"POST", "/url"},
+	{"GET", "/"},
+	{"PUT", "/"},
+}
+
+func TestHandleRequest_invalidRoute_notFoundErrorReturned(t *testing.T) {
+	for _, testCase := range invalidRouteCases {
+		handler := MessageCollectionRequestHandler{}
+		bodyReader := strings.NewReader("body")
+		request := httptest.NewRequest(testCase.method, testCase.url, bodyReader)
+		request.Header.Set("Content-Type", "application/json; charset=utf-8")
+		writer := httptest.NewRecorder()
+
+		handler.HandleRequest(writer, request)
+		response := writer.Result()
+		responseBody, _ := ioutil.ReadAll(response.Body)
+		contentType := response.Header.Get("Content-Type")
+
+		assert.Equal(t, http.StatusNotFound, response.StatusCode)
+		assert.Contains(t, string(responseBody), "title")
+		title := gjson.Get(string(responseBody), "title").String()
+		assert.Contains(t, title, "Route not found")
+		assert.Contains(t, contentType, "application/json")
+	}
+}
+
+func TestHandleRequest_contentTypeIsNotJson_badRequestErrorReturned(t *testing.T) {
+	handler := MessageCollectionRequestHandler{}
+	bodyReader := strings.NewReader("body")
+	request := httptest.NewRequest("POST", "/", bodyReader)
+	request.Header.Set("Content-Type", "text/html; charset=utf-8")
+	writer := httptest.NewRecorder()
+
+	handler.HandleRequest(writer, request)
+	response := writer.Result()
+	responseBody, _ := ioutil.ReadAll(response.Body)
+	contentType := response.Header.Get("Content-Type")
+
+	assert.Equal(t, http.StatusBadRequest, response.StatusCode)
+	assert.Contains(t, string(responseBody), "title")
+	title := gjson.Get(string(responseBody), "title").String()
+	assert.Contains(t, title, "Content type must be application/json")
+	assert.Contains(t, contentType, "application/json")
+}
+
+func TestHandleRequest_validRequest_okReturned(t *testing.T) {
+	handler := MessageCollectionRequestHandler{}
+	bodyReader := strings.NewReader("body")
+	request := httptest.NewRequest("POST", "/", bodyReader)
+	request.Header.Set("Content-Type", "application/json; charset=utf-8")
+	writer := httptest.NewRecorder()
+
+	handler.HandleRequest(writer, request)
+	response := writer.Result()
+	responseBody, _ := ioutil.ReadAll(response.Body)
+	contentType := response.Header.Get("Content-Type")
+
+	assert.Equal(t, http.StatusOK, response.StatusCode)
+	assert.Contains(t, string(responseBody), "title")
+	title := gjson.Get(string(responseBody), "title").String()
+	assert.Contains(t, title, "All messages were successfully sent to queue")
 	assert.Contains(t, contentType, "application/json")
 }
